3f607115de0c54ad1fb87a45606731e91b1ab764
82108 <82108@DESKTOP-PJO9988>
2026-07-21 15:12:24 +0900

    매칭 코드

---
+++ meetory_test/src/main/java/com/meetoy/MeetoryTestApplication.java
@@ -0,0 +1,13 @@
+package com.meetoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetoryTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetoryTestApplication.class, args);
+	}
+
+}
---
+++ meetory_test/src/test/java/com/meetoy/MeetoryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetoryTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

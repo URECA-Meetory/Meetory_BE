844048c21658fa78ce2f01fff631e87dbf647701
lee-suhyeon <162266788+lee-suhyeon@users.noreply.github.com>
2026-07-22 10:47:19 +0900

    브라우저 로그인/ 회원가입
    회원가입, 로그인, 로그아웃 만듬

--- src/main/java/com/meetory/config/SecurityConfig.java
+++ src/main/java/com/meetory/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.meetory.config;
 
 import com.meetory.auth.controller.AuthController;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.meetory.auth.jwt.JwtAuthenticationFilter;
 import com.meetory.auth.jwt.JwtTokenProvider;
@@ -36,10 +41,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) //CORS 추가
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/", "/*.html", "/css/**", "/js/**", "/favicon.ico").permitAll() //정적 페이지 허용
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
@@ -54,6 +61,21 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+    
+    // 브라우저 fetch 요청을 허용하기 위한 CORS 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+    	    CorsConfiguration configuration = new CorsConfiguration();
+    	    configuration.setAllowedOriginPatterns(List.of("http://localhost:8080"));
+    	    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    	    configuration.setAllowedMethods(List.of("*"));
+    	    configuration.setAllowCredentials(true);
+    	    
+    	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    	    source.registerCorsConfiguration("/**", configuration);
+    	    return source;
+    	
     }
 
     @Bean
